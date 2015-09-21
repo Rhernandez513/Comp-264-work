8cda071939c6681762271a86661b14474e8222ca
Robert Hernandez <rhernandez513@gmail.com>
2015-09-21 10:51:36 -0500

    update hw2 to work regardless of word size

--- hws/hw2/big-or-little-endian.c
+++ hws/hw2/big-or-little-endian.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main() {
+  short int short_num = 1;
+  printf("Short int: ");
+  if(*(char *)&short_num == 1) {
+    printf("Little-Endian\n");
+  } else {
+    printf("Big-Endian\n");
+  }
+
   int num = 1;
+  printf("Unspecified int: ");
   if(*(char *)&num == 1) {
+    printf("Little-Endian\n");
+  } else {
+    printf("Big-Endian\n");
+  }
+
+  long int long_num = 1;
+  printf("Long int: ");
+  if(*(char *)&long_num == 1) {
     printf("Little-Endian\n");
   } else {
     printf("Big-Endian\n");
